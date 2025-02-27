7d22392245e1e3acae9ae971551edd94cf385dc7
DEV-COM <kwp@tlen.pl>
2025-02-27 11:08:44 +0100

    11

---
+++ src/test/java/TemplateTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+public class TemplateTest {
+    @TestTemplate
+    @ExtendWith(MyTestTemplateInvocationContextProvider.class)
+    void testTemplate(String parameter){
+        System.out.println(parameter);
+    }
+}
