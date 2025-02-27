f53cb607c02568247d24a0171c5d8dde1f95ee7f
DEV-COM <kwp@tlen.pl>
2025-02-27 10:11:44 +0100

    9

---
+++ src/test/java/CollectionTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CollectionTest {
+
+    @TestFactory
+    Collection<DynamicTest> dynamicTestCollection(){
+        return Arrays.asList(
+                DynamicTest.dynamicTest("1st dynamic test", ()-> assertTrue(true)),
+                DynamicTest.dynamicTest("2nd dynamic test", ()-> assertEquals(4,2+2))
+
+        );
+    }
+    @TestFactory
+    Stream<DynamicTest> dynamicTestStream(){
+        Stream<String> stringStream = Stream.of("A","B","C");
+        return stringStream.map(
+                input -> DynamicTest.dynamicTest("Nazwa testu dla wejścia " + input,
+                        () -> {
+                            System.out.println("Testujemy " + input);
+                        })
+        );
+    }
+}
---
+++ src/test/java/StreamExampleTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.function.ThrowingConsumer;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.DynamicTest.stream;
+
+public class StreamExampleTest {
+
+    @TestFactory
+    Stream<DynamicTest> streamTest(){
+//        dane wejściowe
+        Integer[] array = {1,2,3,4};
+        Iterator<Integer> inputGenerator = Arrays.asList(array).iterator();
+
+//        Nazwa testów
+        Function<Integer, String> displayNameGenerator = (input) -> "Data input: " + input;
+
+//        Text executor
+        ThrowingConsumer<Integer> testExecutor = (input) -> {
+            System.out.println(input);
+            assertEquals(0, input % 2);
+        };
+        return stream(inputGenerator,displayNameGenerator,testExecutor);
+    }
+}
